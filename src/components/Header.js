7edb06a6a4e43b7401195c9e65b8c219976b67a2
antieatingactivist <antieatingactivist@gmail.com>
2022-03-01 23:09:38 -0800

    set up nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+import Nav from './Nav';
+
+
+export default function App() {
+    return (
+      
+        <Nav />
+      
+    );
+  }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,13 @@
 import React from 'react';
 
 export default function Nav() {
-    const linkStyle = { border: '1px black', padding: '5px' };
   
     return (
-      <nav className="main-header-menu">
-        <section
-          style={{
-            display: 'flex',
-            fontFamily: 'helvetica',
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-          }}
-        >
-          <div style={linkStyle}>
-            <a href="#">Home</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Login</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Register</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">About</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Contact</a>
-          </div>
-        </section>
+      <nav>
+            <a href="#about"><span>./</span>About</a>
+            <a href="#projects"><span>./</span>Projects</a>
+            <a href="#contact"><span>./</span>Contact</a>
+            <a href="./assets/pdf/resume.pdf" target="_blank" rel="noopener noreferrer"><span>./</span>Resume</a>
       </nav>
     );
   }

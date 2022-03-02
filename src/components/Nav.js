a6ca7bcb7b54ffc20c76def4d1611b7f7dedbc6c
antieatingactivist <antieatingactivist@gmail.com>
2022-03-02 00:35:47 -0800

    migrated nav style to its component file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 export default function Nav() {
-  
+    const navStyle = {
+        position: 'absolute',
+        padding: '20px',
+        right: 0
+      }
+    const linkPadding = {
+        marginRight: '10px'
+    }
     return (
-      <nav>
-            <a href="#about"><span>./</span>About</a>
-            <a href="#projects"><span>./</span>Projects</a>
-            <a href="#contact"><span>./</span>Contact</a>
-            <a href="./assets/pdf/resume.pdf" target="_blank" rel="noopener noreferrer"><span>./</span>Resume</a>
+      <nav style={navStyle}>
+            <a style={linkPadding} href="#about"><span>./</span>About</a>
+            <a style={linkPadding} href="#projects"><span>./</span>Projects</a>
+            <a style={linkPadding} href="#contact"><span>./</span>Contact</a>
+            <a style={linkPadding} href="./assets/pdf/resume.pdf" target="_blank" rel="noopener noreferrer"><span>./</span>Resume</a>
       </nav>
     );
   }

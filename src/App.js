d6290de450da0b682e5e8b268d79ad1e9c29d2be
antieatingactivist <antieatingactivist@gmail.com>
2022-03-02 22:34:05 -0800

    created intro component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Left from './components/Left';
+import Intro from './components/Intro';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Header />
         <Left />
         <main>
-          
+          <Intro />
         </main>
         <Footer />
         
---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export default function Intro() {
+    return (
+        <section id="intro">
+            <h1>Hi, I'm Garrett.</h1>
+            <h1><span>Developer, and builder of many things.</span></h1>
+            <p>scroll down and have a look.</p>
+        </section>
+    );
+  }

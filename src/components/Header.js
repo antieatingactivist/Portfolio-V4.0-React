2894a94fa3d6b84c47bc0569ea833c53a8d408e0
antieatingactivist <antieatingactivist@gmail.com>
2022-03-04 21:03:50 -0800

    added a project

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ import Nav from './Nav';
 
 
 export default function Header() {
-    let hidden = false;
+
     let style = {
-        top: '-100px',
+        top: '0px',
         border: 'none',
         boxShadow: 'none',
         backgroundColor: '#00000000',
@@ -16,25 +16,56 @@ export default function Header() {
    
 
     const [scrollPosition, setScrollPosition] = useState(0);
+    const [furthestDown, SetFurthestDown] = useState(0);
     const handleScroll = () => {
         const position = window.pageYOffset;
         setScrollPosition(position);
+        // SetFurthestDown(scrollPosition);
         };
-        console.log([scrollPosition]);
-        if (scrollPosition < 4) {
-            style.border = 'none';
-            style.boxShadow = 'none';
-            style.backgroundColor = '#00000000';
-        } else {
-            style.border = null;
+        
+        
+       
+        console.log(furthestDown);
+        
+        if(scrollPosition > furthestDown) {
+            console.log("set");
+            
+            style.top = "-60px";
             style.boxShadow = null;
             style.backgroundColor = null;
+            style.border = null;
+            SetFurthestDown(scrollPosition);
         }
-        if (scrollPosition < 300) {
-            style.top = '0px';
-        } else {
-            style.top = '-100px';
-        }
+        // if(scrollPosition < furthestDown-100) {
+            
+        //     console.log("cleared");
+        //     // style.top = "0px";
+        //     SetFurthestDown(scrollPosition);
+        // }
+        
+        // if (scrollPosition < 60) {
+            
+        //     style.boxShadow = "none";
+        //     style.backgroundColor = "#00000000";
+        //     style.border = "none";
+        // }
+        // if (scrollPosition < 140) {
+        //     style.top = null;
+        // }   
+        // if (scrollPosition < 4) {
+        //     style.border = 'none';
+        //     style.boxShadow = 'none';
+        //     style.backgroundColor = '#00000000';
+        // } else {
+        //     style.border = null;
+        //     style.boxShadow = null;
+        //     style.backgroundColor = null;
+        // }
+        // if (scrollPosition < 300) {
+        //     style.top = '0px';
+        // } else {
+        //     style.top = '-100px';
+        // }
 
         useEffect(() => {
         window.addEventListener("scroll", handleScroll);

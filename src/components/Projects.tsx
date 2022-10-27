a60fb60d0711de8aa590d350f858c1c5e36c81e6
antieatingactivist <antieatingactivist@gmail.com>
2022-10-27 00:02:49 -0700

    changed spacing above skills

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -12,7 +12,7 @@ export default function Projects({scrollPosition, windowHeight, windowWidth, set
 
     const projectsStyle = {
         transition: 'all 2s',
-        padding: windowWidth < 500 ? '35vh 10px 20vh 10px' : '35vh 10px 20vh 10px'
+        padding: windowWidth < 500 ? '10vh 10px 20vh 10px' : '10vh 10px 20vh 10px'
     }
   
     return (
